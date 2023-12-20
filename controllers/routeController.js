3d9eb1ca1311cf4f84bec4adf1136272413cd123
johnthezero <m.jones@live.fr>
2023-12-20 11:28:54 +0100

    f
    f

--- controllers/routeController.js
+++ controllers/routeController.js
@@ -1,7 +1,5 @@
-
-
 const jwt = require("jsonwebtoken");
-const bcrypt = require("bcrypt");
+const bcrypt = require("bcryptjs");
 const User=require("../models/User");
 const Job=require("../models/Job");
 const { ObjectId } = require("mongodb");
