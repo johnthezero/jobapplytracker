9b26f30d2868460c9669dcbda8b401633cb22782
johnthezero <m.jones@live.fr>
2023-12-20 12:16:05 +0100

    Update app.js

--- app.js
+++ app.js
@@ -23,7 +23,7 @@ app.set("views",path.join(__dirname,"views"));
 // LINK TO DB ↓ ▼ ↓
 
 
-const dbURI =  `mongodb+srv://jonthezero:${process.env.MONGO_URI}@cluster0.9rolcn7.mongodb.net/?retryWrites=true&w=majority`;
+const dbURI =  `mongodb+srv://jonthezero:${process.env.MONGO_URI}@cluster0.9rolcn7.mongodb.net/`;
 
 mongoose.connect(dbURI)
   .then((result) => {
